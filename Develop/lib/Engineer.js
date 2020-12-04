f174bbd7c449b26e5a0d3b2ec7f7a1af574eb504
joolyenIV <julianivey@gmail.com>
2020-12-04 16:34:48 -0600

    building employee classes

--- Develop/lib/Engineer.js
+++ Develop/lib/Engineer.js
@@ -1,1 +1,17 @@
-// TODO: Write code to define and export the Engineer class.  HINT: This class should inherit from Employee.
+// code to define and export the Engineer class. This class inherits from Employee.
+const Employee = require("./Employee")
+
+class Engineer extends Employee {
+    constructor(name, id, email, github){
+        //need to use super here to get properties from Employee class we're extended
+        super(name, id, email);
+        this.title = "Engineer";
+        this.github = github;
+    }
+    //only need to create a function for the new property added
+    getOfficeNumber(){
+        return this.github;
+    }
+}
+
+module.exports = Engineer
