095750e2d5e31e43b2fd12cab3ffaf8266335acf
joolyenIV <julianivey@gmail.com>
2020-12-04 16:32:20 -0600

    building employee classes

--- Develop/lib/Manager.js
+++ Develop/lib/Manager.js
@@ -2,7 +2,16 @@
 const Employee = require("./Employee")
 
 class Manager extends Employee {
-
+    constructor(name, id, email, officeNumber){
+        //need to use super here to get properties from Employee class we're extended
+        super(name, id, email);
+        this.title = "Manager";
+        this.officeNumber = officeNumber;
+    }
+    //only need to create a function for the new property added
+    getOfficeNumber(){
+        return this.officeNumber;
+    }
 }
 
 module.exports = Manager
