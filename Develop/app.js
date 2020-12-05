b55f2c7f03ba78704e85f5ce275295a608ce6643
joolyenIV <julianivey@gmail.com>
2020-12-04 18:02:52 -0600

    pretty close on the app.js file, still getting an error but I need to save to repo

--- Develop/app.js
+++ Develop/app.js
@@ -9,27 +9,157 @@ const OUTPUT_DIR = path.resolve(__dirname, "output");
 const outputPath = path.join(OUTPUT_DIR, "team.html");
 
 const render = require("./lib/htmlRenderer");
+const team = [];
 
+getManager();
 
-// Write code to use inquirer to gather information about the development team members,
-// and to create objects for each team member (using the correct classes as blueprints!)
+function getManager() {
+    return inquirer
+        .prompt([
+            {
+                type: "input",
+                name: "managerName",
+                message: "Which manager would you like on this team?"
+            },
+            {
+                type: "input",
+                name: "managerId",
+                message: "What is the manager's id?"
+            },
+            {
+                type: "input",
+                name: "managerEmail",
+                message: "What is the manager's email?",
+                default: () => {},
+                validate: function (email) {
+                    //adding email validation to the form
+                    valid = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(email)
+                    if (valid) {
+                        return true;
+                    } else {
+                        console.log(".  Please enter a valid email")
+                        return false;
+                    }
+                }
+            },
+            {
+                type: "input",
+                name: "managerOffice",
+                message: "What is the manager's office number?"
+            },
+        ]).then(answers => {
+            
+            //creating a new instance based off of the users responses and adding it to the array
+            const manager = new Manager(answers.managerName, answers.managerID, answers.managerEmail, answers.managerOffice);
+            team.push(manager);
+            addEmployee();
+        })
+}
 
-// After the user has input all employees desired, call the `render` function (required
-// above) and pass in an array containing all employee objects; the `render` function will
-// generate and return a block of HTML including templated divs for each employee!
+function addEmployee() {
+    return inquirer
+        .prompt([
+            {
+                type: "list",
+                name: "choice",
+                message: "Would you like to add another person to this team?",
+                choices: ["Engineer", "Intern", "No Thanks"]
+            }
+        ]).then(answers => {
+            //using a switch statement to choose team members to add
+            switch (answers.choice) {
+                case "Engineer":
+                    createEngineer();
+                    break;
+                case "Intern":
+                    createIntern();
+                    break;
+                default:
+                    createTeam();
+            }
+        })
+}
 
-// After you have your html, you're now ready to create an HTML file using the HTML
-// returned from the `render` function. Now write it to a file named `team.html` in the
-// `output` folder. You can use the variable `outputPath` above target this location.
-// Hint: you may need to check if the `output` folder exists and create it if it
-// does not.
+function createEngineer() {
+    return inquirer
+    .prompt([
+        {
+            type: "input",
+            name: "engineerName",
+            message: "What is the name of the Engineer?"
+        },
+        {
+            type: "input",
+            name: "engineerID",
+            message: "What is your Engineer's id?"
+        },
+        {
+            type: "input",
+            name: "engineerEmail",
+            message: "What is your Engineer's email?",
+            default: () => {},
+            validate: function (email) {
+                valid = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(email)
+                if (valid) {
+                    return true;
+                } else {
+                    console.log(".  Please enter a valid email")
+                    return false;
+                }
+            }
+        },
+        {
+            type: "input",
+            name: "engineerGitHub",
+            message: "What is your Engineer's GitHub username?"
+        }
+    ]).then(answers => {
+        const engineer = new Engineer(answers.engineerName, answers.engineerID, answers.engineerEmail, answers.engineerGitHub);
+        team.push(engineer);
+        addEmployee();
+    })
+}
 
-// HINT: each employee type (manager, engineer, or intern) has slightly different
-// information; write your code to ask different questions via inquirer depending on
-// employee type.
+function createIntern() {
+    return inquirer
+    .prompt([
+        {
+            type: "input",
+            name: "internName",
+            message: "What is the name of the Intern?"
+        },
+        {
+            type: "input",
+            name: "internID",
+            message: "What is your Intern's id?"
+        },
+        {
+            type: "input",
+            name: "internEmail",
+            message: "What is your Intern's email?",
+            default: () => {},
+            validate: function (email) {
+                valid = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(email)
+                if (valid) {
+                    return true;
+                } else {
+                    console.log(".  Please enter a valid email")
+                    return false;
+                }
+            }
+        },
+        {
+            type: "input",
+            name: "internSchool",
+            message: "What is your Intern's school?"
+        }
+    ]).then(answers => {
+        const intern = new Intern(answers.internName, answers.internID, answers.internEmail, answers.internSchool);
+        team.push(intern);
+        addEmployee();
+    })
+}
 
-// HINT: make sure to build out your classes first! Remember that your Manager, Engineer,
-// and Intern classes should all extend from a class named Employee; see the directions
-// for further information. Be sure to test out each class and verify it generates an
-// object with the correct structure and methods. This structure will be crucial in order
-// for the provided `render` function to work! ```
+function createTeam() {
+    fs.writeFileSync(outputPath, render(team));
+}
